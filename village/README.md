fb73a5085eefc04a2f11e036cb2d7b999d58d8b6
LeOtaku <leo.gaskin@brg-feldkirchen.at>
2019-03-16 16:23:04 +0100

    add readme and village

---
+++ village/README.md
@@ -0,0 +1,13 @@
+# village - predefined elf configurations to choose from.
+
+Elf prompt configurations can easily be packaged separately from your other ZSH configurations.
+
+You may load them in the following manner:
+
+```
+source $VILLAGE_SCRIPTDIR/your_favorite_elf.zsh
+elf_prompt_initialize
+#elf_both_initialize
+```
+
+Elves usually live in villages, right?
---
+++ village/elv.zsh
@@ -0,0 +1,13 @@
+# the default prompt used by the elvish shell, 
+# but with fish-style directory collapsing
+
+ELF_SCRIPTDIR="$(dirname $(realpath $0))/../"
+source $ELF_SCRIPTDIR/elf.zsh
+
+elf_add la '${${PWD/$HOME/~}//(#b)(\/#.#?)[^\/]#\//$match[1]/}> '
+elf_add ra '%{$fg[black]$bg[white]%}'
+elf_add ra '%n@%m'
+elf_add ra '%{$fg[default]$bg[default]%}'
+
+ELF_FINISHED_PROMPT="$ELF_PROMPT"
+ELF_FINISHED_RPROMPT=""
