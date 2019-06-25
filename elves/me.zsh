ba82c0ab5d93cfaac06aa1367f508fb650f43287
LeOtaku <leo.gaskin@brg-feldkirchen.at>
2019-06-25 18:51:40 +0200

    remove unused jobs functionality

--- elves/me.zsh
+++ elves/me.zsh
@@ -25,24 +25,3 @@ function me_nix_prompt {
 ELF_FINISHED_PROMPT="$ELF_PROMPT"
 ELF_FINISHED_RPROMPT=""
 
-# typeset -g JOBS
-# 
-# function set_rprompt {
-#     local jobn
-# 
-#     jobn="${(S%%)${:-%j}}"
-# 
-#     if [[ "$jobn" != "$OLD_JOBN" ]]; then
-#         if [[ "$jobn" == 0 ]]; then
-#             JOBS=""
-#         else
-#             JOBS="%F{yellow}%B$jobn%b"
-#         fi
-#         zle && zle .reset-prompt
-#         typeset -g OLD_JOBN="$jobn"
-#     fi
-#     
-#     aui_start_worker $1
-#     aui_run_worker $1 "sleep 0.2" "set_rprompt"
-# }
-
